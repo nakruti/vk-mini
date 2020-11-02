18e3327c73e28908f920a613dc3b894c3c9472e7
nakruti <68735236+nakruti@users.noreply.github.com>
2020-11-02 21:28:41 +0300

    Add files via upload

---
+++ precache-manifest.50dc2692a529c8c0cbbf1f3c2ecdd2bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf496fd26ec4f6efcc0365a9f208b9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0574192717d94fedd9da",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "008c3c2f12d9e63cbf61",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0574192717d94fedd9da",
+    "url": "/static/js/2.ea482698.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.ea482698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008c3c2f12d9e63cbf61",
+    "url": "/static/js/main.0216a8b6.chunk.js"
+  },
+  {
+    "revision": "7698b597cfb31c0bf436",
+    "url": "/static/js/runtime-main.4fec95a9.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
